7fea3cff51a4fc9d6b401a2ccf277ad38b007b26
Anthony C <47967077+ant-car@users.noreply.github.com>
2024-03-03 12:45:22 +1100

    Add TouchPanel functionality

---
+++ TouchPanel/CrestronPanel.cs
@@ -0,0 +1,78 @@
+﻿using Crestron.SimplSharpPro;
+using Crestron.SimplSharpPro.DeviceSupport;
+
+namespace AVCoders.Crestron.TouchPanel;
+
+public static class CrestronPanel
+{
+    public static void Interlock(BasicTriList panel, uint active, uint[] all)
+    {
+        foreach (uint join in all)
+        {
+            panel.BooleanInput[join].BoolValue = join == active;
+        }
+    }
+
+    public static void TogglingInterlock(BasicTriList panel, uint active, uint[] all)
+    {
+        foreach (uint join in all)
+        {
+            if (join == active)
+                panel.BooleanInput[join].BoolValue = !panel.BooleanInput[join].BoolValue;
+            else
+                panel.BooleanInput[join].BoolValue = join == active;
+        }
+    }
+
+    public static void Interlock(SmartObject smartObject, uint active, uint[] all)
+    {
+        foreach (uint join in all)
+        {
+            if (smartObject.BooleanInput.Contains(join))
+            {
+                smartObject.BooleanInput[join].BoolValue = join == active;
+            }
+        }
+    }
+
+    public static void TogglingInterlock(SmartObject smartObject, uint active, uint[] all)
+    {
+        foreach (uint join in all)
+        {
+            if (smartObject.BooleanInput.Contains(join))
+            {
+                if (join == active)
+                    smartObject.BooleanInput[join].BoolValue = !smartObject.BooleanInput[join].BoolValue;
+                else
+                    smartObject.BooleanInput[join].BoolValue = join == active;
+            }
+        }
+    }
+
+    public static void Interlock(List<BasicTriListWithSmartObject> panels, uint active, uint[] all)
+    {
+        panels.ForEach(panel => Interlock(panel, active, all));
+    }
+
+    public static void Interlock(List<SmartObject> smartObjects, uint active, uint[] all)
+    {
+        smartObjects.ForEach(smartObject => Interlock(smartObject, active, all));
+    }
+
+    public static void Interlock(List<BasicTriList> panels , uint active, uint[] all)
+    {
+        panels.ForEach(panel => Interlock(panel, active, all));
+    }
+
+    public static void TogglingInterlock(List<BasicTriList> panels, uint active, uint[] all)
+    {
+        uint newActive = panels.First().BooleanInput[active].BoolValue? 0 : active;
+        panels.ForEach(x => Interlock(x, newActive, all));
+    }
+
+    public static void TogglingInterlock(List<BasicTriListWithSmartObject> panels, uint active, uint[] all)
+    {
+        uint newActive = panels.First().BooleanInput[active].BoolValue? 0 : active;
+        panels.ForEach(x => Interlock(x, newActive, all));
+    }
+}
---
+++ TouchPanel/Notifications.cs
@@ -0,0 +1,63 @@
+﻿using Crestron.SimplSharp;
+using Crestron.SimplSharpPro;
+using Crestron.SimplSharpPro.DeviceSupport;
+
+namespace AVCoders.Crestron.TouchPanel;
+
+public record NotificationJoins(uint Notify, uint Warn, uint[] RelatedPages, uint MessageText, uint Close);
+
+public class Notifications
+{
+    private readonly NotificationJoins _joins;
+    private readonly BasicTriList _panel;
+    private Guid _currentWarning;
+
+    public Notifications(BasicTriList panel, NotificationJoins joins)
+    {
+        _joins = joins;
+        _panel = panel;
+        _panel.SigChange += PanelButtonPressed;
+    }
+
+    public void Notify(string message, int timeout = 3000)
+    {
+        _panel.StringInput[_joins.MessageText].StringValue = message;
+        SetBanner(_joins.Notify);
+        new CTimer(x => SetBanner(0), timeout);
+    }
+
+    public Guid Warn(string message)
+    {
+        _currentWarning = Guid.NewGuid();
+        _panel.StringInput[_joins.MessageText].StringValue = message;
+        SetBanner(_joins.Warn);
+        return _currentWarning;
+    }
+
+    public void ClearWarning(Guid wanringId)
+    {
+        if (_currentWarning == wanringId)
+        {
+            SetBanner(0);
+            _currentWarning = Guid.Empty;
+        }
+    }
+
+    private void PanelButtonPressed(BasicTriList currentDevice, SigEventArgs args)
+    {
+        if (args.Sig.Type != eSigType.Bool)
+            return;
+        if (args.Sig.BoolValue == false)
+            return;
+        if (args.Sig.Number == _joins.Close)
+        {
+            SetBanner(0);
+            _currentWarning = Guid.Empty;
+        }
+    }
+
+    private void SetBanner(uint activeBanner)
+    {
+        CrestronPanel.Interlock(_panel, activeBanner, _joins.RelatedPages);
+    }
+}
