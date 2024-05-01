61314ce120bd56b04c17f384618ea6db281f681e
Anthony C <47967077+ant-car@users.noreply.github.com>
2024-05-01 12:50:44 +1000

    Add Tab Button Helper

---
+++ SmartGraphics/TabButtonHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace AVCoders.Crestron.SmartGraphics;
+
+public static class TabButtonHelper
+{
+    public static uint GetButtonId(uint sigNumber)
+    {
+        return (sigNumber + 1) / 2;
+    }
+
+    public static uint GetFeedbackSignal(uint buttonIndex)
+    {
+        return buttonIndex * 2;
+    }
+}
---
+++ SmartGraphicsTest/TabButtonHelperTest.cs
@@ -0,0 +1,30 @@
+﻿namespace AVCoders.Crestron.SmartGraphics.Tests;
+
+public class TabButtonHelperTest
+{
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(3, 2)]
+    [InlineData(5, 3)]
+    [InlineData(7, 4)]
+    [InlineData(9, 5)]
+    [InlineData(11, 6)]
+    [InlineData(13, 7)]
+    public void GetButtonId_ReturnsTheButtonId(uint input, uint expected)
+    {
+        Assert.Equal(expected, TabButtonHelper.GetButtonId(input));
+    }
+    
+    [Theory]
+    [InlineData(2, 1)]
+    [InlineData(4, 2)]
+    [InlineData(6, 3)]
+    [InlineData(8, 4)]
+    [InlineData(10, 5)]
+    [InlineData(12, 6)]
+    [InlineData(14, 7)]
+    public void GetFeedbackSignal_ReturnsTheSignalNumber(uint expected, uint input)
+    {
+        Assert.Equal(expected, TabButtonHelper.GetFeedbackSignal(input));
+    }
+}
