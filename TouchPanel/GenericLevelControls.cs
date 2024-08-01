49c955d36ce780f093c5e07229b4b2fb01572d14
Anthony C <47967077+ant-car@users.noreply.github.com>
2024-08-02 09:34:54 +1000

    Create combined volume controls

---
+++ TouchPanel/GenericLevelControls.cs
@@ -0,0 +1,69 @@
+﻿using AVCoders.Dsp;
+
+namespace AVCoders.Crestron.TouchPanel;
+
+public class GenericLevelControls : LevelControls
+{
+    private readonly List<UIFader> _faders;
+
+    public GenericLevelControls(string name, List<UIFader> faders, List<SmartObject> smartObjects, uint joinIncrement = DefaultJoinIncrement) :
+        base(name, (ushort)faders.Count, smartObjects, joinIncrement)
+    {
+        _faders = faders;
+
+        for (int i = 0; i < faders.Count; i++)
+        {
+            Log($"Setting up fader {i}");
+            var faderIndex = i;
+            _faders[i].VolumeLevelHandlers += volumeLevel => HandleVolumeLevel(volumeLevel, faderIndex); 
+            _faders[i].MuteStateHandlers += muteState => HandleMuteState(muteState, faderIndex);
+            SmartObjects.ForEach(smartObject => smartObject.StringInput[SrlHelper.SerialJoinFor(i, NameJoin)].StringValue = faders[i].Name);
+        }
+    }
+
+    protected override void HandleVolumePress(GenericBase currentDevice, SmartObjectEventArgs args)
+    {
+        var selectionInfo = SrlHelper.GetSigInfo(args.Sig);
+        switch (args.Sig.Type)
+        {
+            // Some touch panels send a sig 1 event as well as the button press event.
+            case eSigType.Bool when args.Sig.Number < 4000:
+                return;
+            case eSigType.Bool when !args.Sig.BoolValue:
+                ButtonHeld = false;
+                return;
+            case eSigType.Bool when selectionInfo.Join > 3:
+                Log($"Ignoring button press {args.Sig.Number}");
+                return;
+            case eSigType.Bool:
+            {
+                Log($"Volume Button pressed, id {args.Sig.Number}.  Index {selectionInfo.Index}, Join: {selectionInfo.Join}");
+                switch (selectionInfo.Join)
+                {
+                    case VolumeUpJoin:
+                    {
+                        void Action() => _faders[selectionInfo.Index].LevelUp(2);
+                        VolumeControl(Action);
+                        Log($"Queued volume up for fader {_faders[selectionInfo.Index].Name}");
+                        break;
+                    }
+                    case VolumeDownJoin:
+                    {
+                        void Action() => _faders[selectionInfo.Index].LevelDown(2);
+                        VolumeControl(Action);
+                        Log($"Queued volume down for fader {_faders[selectionInfo.Index].Name}");
+                        break;
+                    }
+                    case MuteJoin:
+                        _faders[selectionInfo.Index].ToggleAudioMute();
+                        Log($"Toggled mute for fader {_faders[selectionInfo.Index].Name}");
+                        break;
+                }
+                break;
+            }
+            case eSigType.UShort:
+                _faders[selectionInfo.Index].SetLevel(args.Sig.ShortValue);
+                break;
+        }
+    }
+}
---
+++ TouchPanel/UiFader.cs
@@ -0,0 +1,101 @@
+using AVCoders.Core;
+using AVCoders.Dsp;
+using AVCoders.Display;
+
+namespace AVCoders.Crestron.TouchPanel;
+
+public abstract class UIFader
+{
+    public readonly string Name;
+    public VolumeLevelHandler? VolumeLevelHandlers;
+    public MuteStateHandler? MuteStateHandlers;
+
+    protected UIFader(string name)
+    {
+        Name = name;
+    }
+
+    public abstract void LevelUp(int amount);
+
+    public abstract void LevelDown(int amount);
+
+    public abstract void SetLevel(int percentage);
+
+    public abstract void ToggleAudioMute();
+
+    public abstract void SetAudioMute(MuteState state);
+}
+
+public class QscFader : UIFader
+{
+    private readonly string _levelNamedControl;
+    private readonly string _muteNamedControl;
+    private readonly QsysEcp _dsp;
+
+    public QscFader(QscAudioBlockInfo audioBlockInfo, QsysEcp dsp) : base(audioBlockInfo.Name)
+    {
+        _dsp = dsp;
+        _levelNamedControl = audioBlockInfo.LevelInstanceTag;
+        _muteNamedControl = audioBlockInfo.MuteInstanceTag;
+        _dsp.AddControl(volumeLevel => VolumeLevelHandlers?.Invoke(volumeLevel), _levelNamedControl);
+        _dsp.AddControl(muteState => MuteStateHandlers?.Invoke(muteState), _muteNamedControl);
+    }
+    
+    public override void LevelUp(int amount) => _dsp.LevelUp(_levelNamedControl, amount);
+
+    public override void LevelDown(int amount) => _dsp.LevelDown(_levelNamedControl, amount);
+
+    public override void SetLevel(int percentage) => _dsp.SetLevel(_levelNamedControl, percentage);
+
+    public override void ToggleAudioMute() => _dsp.ToggleAudioMute(_muteNamedControl);
+    
+    public override void SetAudioMute(MuteState state) => _dsp.SetAudioMute(_muteNamedControl, state);
+}
+
+public class BiampFader : UIFader
+{
+    private readonly string _instanceTag;
+    private readonly int _index;
+    private readonly BiampTtp _dsp;
+
+    public BiampFader(BiampAudioBlockInfo audioBlockInfo, BiampTtp dsp) : base(audioBlockInfo.Name)
+    {
+        _dsp = dsp;
+        _instanceTag = audioBlockInfo.InstanceTag;
+        _index = audioBlockInfo.BlockIndex;
+        
+        _dsp.AddControl(volumeLevel => VolumeLevelHandlers?.Invoke(volumeLevel), _instanceTag, _index);
+        _dsp.AddControl(muteState => MuteStateHandlers?.Invoke(muteState), _instanceTag, _index);
+    }
+    public override void LevelUp(int amount) => _dsp.LevelUp(_instanceTag, _index, amount);
+
+    public override void LevelDown(int amount) => _dsp.LevelDown(_instanceTag, _index, amount);
+
+    public override void SetLevel(int percentage) => _dsp.SetLevel(_instanceTag, _index, percentage);
+
+    public override void ToggleAudioMute() => _dsp.ToggleAudioMute(_instanceTag, _index);
+    
+    public override void SetAudioMute(MuteState state) => _dsp.SetAudioMute(_instanceTag, state);
+}
+
+public class DisplayFader : UIFader
+{
+    private readonly Display.Display _display;
+
+    public DisplayFader(DisplayInfo displayInfo) : base(displayInfo.Name)
+    {
+        _display = displayInfo.Display;
+        _display.VolumeLevelHandlers += volumeLevel => VolumeLevelHandlers?.Invoke(volumeLevel);
+        _display.MuteStateHandlers += muteState => MuteStateHandlers?.Invoke(muteState);
+    }
+
+    public override void LevelUp(int amount) => _display.SetVolume(_display.GetCurrentVolume() + amount);
+
+    public override void LevelDown(int amount) => _display.SetVolume(_display.GetCurrentVolume() - amount);
+
+    public override void SetLevel(int percentage) => _display.SetVolume(percentage);
+
+    public override void ToggleAudioMute() => _display.ToggleAudioMute();
+    
+    public override void SetAudioMute(MuteState state) => _display.SetAudioMute(state);
+}
