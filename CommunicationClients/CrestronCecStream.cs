c40880bd37ae627a5c316ebb39522172d6f4e48e
Anthony C <47967077+ant-car@users.noreply.github.com>
2024-08-27 17:53:21 +1000

    Create CrestronCecStream CEC Client

---
+++ CommunicationClients/CrestronCecStream.cs
@@ -0,0 +1,25 @@
+using AVCoders.Core;
+using Crestron.SimplSharpPro.DM;
+
+namespace AVCoders.Crestron.CommunicationClients;
+
+public class CrestronCecStream: SerialClient
+{
+    private readonly Cec _stream;
+
+    public CrestronCecStream(Cec stream)
+    {
+        _stream = stream;
+        _stream.CecChange += HandleCecResponse;
+    }
+
+    private void HandleCecResponse(Cec cecDevice, CecEventArgs args) => ResponseHandlers?.Invoke(cecDevice.Received.StringValue);
+
+    public override void Send(string message) => _stream.Send.StringValue = message;
+
+    public override void Send(byte[] bytes) => _stream.Send.CharacterArrayValue = bytes.Select(b => (char)b).ToArray();
+
+    public override void ConfigurePort(SerialSpec serialSpec) => Log("This port can't be configured", EventLevel.Error);
+
+    public override void Send(char[] chars) => _stream.Send.CharacterArrayValue = chars;
+}
