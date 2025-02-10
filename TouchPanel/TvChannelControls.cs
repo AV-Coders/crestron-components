1d6d5516a42519cbc242c77194326098598ee367
Anthony C <47967077+ant-car@users.noreply.github.com>
2025-02-11 00:13:07 +1100

    Add press and hold to d-pad and volume buttons

--- TouchPanel/TvChannelControls.cs
+++ TouchPanel/TvChannelControls.cs
@@ -2,60 +2,90 @@
 
 namespace AVCoders.Crestron.TouchPanel;
 
+public record TVControlActionDetails(Action Action, bool Repeat);
+
 public class TvChannelControls
 {
     private readonly ISetTopBox _stb;
     private readonly string _name;
+    private CancellationTokenSource _pressAndHoldToken = new ();
     
-    private readonly Dictionary<uint, Action> _actions = new ();
+    private readonly Dictionary<uint, TVControlActionDetails> _actions = new ();
 
     public TvChannelControls(List<SmartObject> smartObjects, ISetTopBox stb, string name)
     {
         _stb = stb;
         _name = name;
         
-        smartObjects.ForEach(x => x.SigChange += ExterityButtonPressed);
+        smartObjects.ForEach(x => x.SigChange += SetTopBoxButtonPressed);
         
-        _actions.Add(1001, _stb.ChannelUp);
-        _actions.Add(1002, _stb.ChannelDown);
-        _actions.Add(1003, () => _stb.SendIRCode(RemoteButton.VolumeUp));
-        _actions.Add(1004, () => _stb.SendIRCode(RemoteButton.VolumeDown));
+        _actions.Add(1001, new TVControlActionDetails(_stb.ChannelUp, true));
+        _actions.Add(1002, new TVControlActionDetails(_stb.ChannelDown, true));
+        _actions.Add(1003, new TVControlActionDetails(() => _stb.SendIRCode(RemoteButton.VolumeUp), true));
+        _actions.Add(1004, new TVControlActionDetails(() => _stb.SendIRCode(RemoteButton.VolumeDown), true));
         
-        _actions.Add(1011, () => _stb.SendIRCode(RemoteButton.Up));
-        _actions.Add(1012, () => _stb.SendIRCode(RemoteButton.Down));
-        _actions.Add(1013, () => _stb.SendIRCode(RemoteButton.Left));
-        _actions.Add(1014, () => _stb.SendIRCode(RemoteButton.Right));
-        _actions.Add(1015, () => _stb.SendIRCode(RemoteButton.Enter));
-        _actions.Add(1016, () => _stb.SendIRCode(RemoteButton.Back));
+        _actions.Add(1011, new TVControlActionDetails(() => _stb.SendIRCode(RemoteButton.Up), true));
+        _actions.Add(1012, new TVControlActionDetails(() => _stb.SendIRCode(RemoteButton.Down), true));
+        _actions.Add(1013, new TVControlActionDetails(() => _stb.SendIRCode(RemoteButton.Left), true));
+        _actions.Add(1014, new TVControlActionDetails(() => _stb.SendIRCode(RemoteButton.Right), true));
+        _actions.Add(1015, new TVControlActionDetails(() => _stb.SendIRCode(RemoteButton.Enter), true));
+        _actions.Add(1016, new TVControlActionDetails(() => _stb.SendIRCode(RemoteButton.Back), true));
         
-        _actions.Add(1021, () => _stb.SendIRCode(RemoteButton.Power));
-        _actions.Add(1022, _stb.ToggleSubtitles);
+        _actions.Add(1021, new TVControlActionDetails(() => _stb.SendIRCode(RemoteButton.Power), false));
+        _actions.Add(1022, new TVControlActionDetails(_stb.ToggleSubtitles, false));
     }
 
-    private void ExterityButtonPressed(GenericBase currentDevice, SmartObjectEventArgs args)
+    private void SetTopBoxButtonPressed(GenericBase currentDevice, SmartObjectEventArgs args)
     {
+        if (!CrestronPanel.EventIsAButtonPress(args))
+        {
+            _pressAndHoldToken.Cancel();
+            return;
+        }
         if (args.Sig.Number < 11)
-            return;
-        if (args.Sig.Type != eSigType.Bool)
-            return;
-        if (args.Sig.BoolValue != true)
             return;
         
         uint button = args.Sig.Number - 4010;
         Log($"Button {button} Pressed");
         
         if(_actions.TryGetValue(button, out var action))
-            action.Invoke();
+        {
+            if (action.Repeat)
+            {
+                _pressAndHoldToken = new CancellationTokenSource();
+                new Task(() => SimulatePressAndHold(action.Action)).Start();
+            }
+            else
+                action.Action.Invoke();
+        }
         else
         {
             _stb.SetChannel((int)button);
             Log($"Channel {button}");
         }
     }
+    
+    private void SimulatePressAndHold(Action action)
+    {
+        try
+        {
+            action.Invoke();
+            Task.Delay(400, _pressAndHoldToken.Token).Wait();
+            while (!_pressAndHoldToken.IsCancellationRequested)
+            {
+                action.Invoke();
+                Task.Delay(150, _pressAndHoldToken.Token).Wait();
+            }
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+        }
+    }
 
     private void Log(string message)
     {
-        CrestronConsole.PrintLine($"{_name} - TvChannelControls - {message}");
+        CrestronConsole.PrintLine($"{DateTime.Now} - {_name} - TvChannelControls - {message}");
     }
     
 }
