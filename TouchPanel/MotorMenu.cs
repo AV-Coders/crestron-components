1430e3d4c20005bb5ea03a6c8595991607f7af0d
Anthony C <47967077+ant-car@users.noreply.github.com>
2025-03-11 12:34:45 +1100

    Add motor-type support, fix joins

--- TouchPanel/MotorMenu.cs
+++ TouchPanel/MotorMenu.cs
@@ -2,9 +2,18 @@ using AVCoders.Crestron.SmartGraphics;
 
 namespace AVCoders.Crestron.TouchPanel;
 
+public enum MotorType
+{
+    Unknown,
+    Roller,
+    Curtain
+}
+public record MotorInfo(Motor.Motor Motor, MotorType Type);
+
+
 public class MotorMenu
 {
-    private readonly List<Motor.Motor> _motors;
+    private readonly List<MotorInfo> _motors;
     private readonly SubpageReferenceListHelper _srlHelper;
     private readonly string _name;
 
@@ -12,9 +21,9 @@ public class MotorMenu
     private const uint LowerJoin = 2;
     private const uint StopJoin = 3;
 
-    private const uint NameJoin = 1;
+    private const uint NameJoin = 4;
 
-    public MotorMenu(string name, List<Motor.Motor> motors, List<SmartObject> smartObjects)
+    public MotorMenu(string name, List<MotorInfo> motors, List<SmartObject> smartObjects)
     {
         _name = name;
         _motors = motors;
@@ -26,7 +35,27 @@ public class MotorMenu
             smartObject.SigChange += HandleMotorPress;
             for (int i = 0; i < _motors.Count; i++)
             {
-                smartObject.StringInput[_srlHelper.SerialJoinFor(i, NameJoin)].StringValue = _motors[i].Name;
+                smartObject.StringInput[_srlHelper.SerialJoinFor(i, NameJoin)].StringValue = _motors[i].Motor.Name;
+                smartObject.StringInput[_srlHelper.SerialJoinFor(i, RaiseJoin)].StringValue = _motors[i].Type switch
+                {
+                    MotorType.Roller => "Up",
+                    MotorType.Curtain => "Open",
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+                smartObject.StringInput[_srlHelper.SerialJoinFor(i, RaiseJoin)].StringValue = _motors[i].Type switch
+                {
+                    MotorType.Roller => "Down",
+                    MotorType.Curtain => "Close",
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+                ushort iconMode = _motors[i].Type switch
+                {
+                    MotorType.Roller => 0,
+                    MotorType.Curtain => 1,
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+                smartObject.UShortInput[_srlHelper.AnalogJoinFor(i, RaiseJoin)].UShortValue = iconMode;
+                smartObject.UShortInput[_srlHelper.AnalogJoinFor(i, LowerJoin)].UShortValue = iconMode;
             }
         });
     }
@@ -42,15 +71,15 @@ public class MotorMenu
         switch (joinInfo.Join)
         {
             case RaiseJoin:
-                _motors[joinInfo.Index].Raise();
+                _motors[joinInfo.Index].Motor.Raise();
                 Log($"Raising");
                 break;
             case LowerJoin:
-                _motors[joinInfo.Index].Lower();
+                _motors[joinInfo.Index].Motor.Lower();
                 Log($"Lowering");
                 break;
             case StopJoin:
-                _motors[joinInfo.Index].Stop();
+                _motors[joinInfo.Index].Motor.Stop();
                 Log($"Stopping");
                 break;
         }
