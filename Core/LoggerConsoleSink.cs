8f44b29d3f8c0dfb2dec472b5c05f3995a390fc3
Anthony C <47967077+ant-car@users.noreply.github.com>
2025-03-24 20:29:53 +1100

    Create a very basic Crestron Sink

---
+++ Core/LoggerConsoleSink.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using AVCoders.Core;
+using Crestron.SimplSharp;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace AVCoders.Crestron.Core;
+
+public abstract class SinkBase : ILogEventSink
+{
+    public void Emit(LogEvent logEvent)
+    {
+        StringBuilder sb = new StringBuilder("[");
+        sb.Append(DateTime.Now);
+        sb.Append(" | ");
+        sb.Append(logEvent.Level.ToString());
+        sb.Append("] ");
+        if (logEvent.Properties.TryGetValue("Class", out var c))
+        {
+            sb.Append(c.ToString().Replace("\"", null));
+            sb.Append(" - ");
+        }
+        if (logEvent.Properties.TryGetValue("InstanceName", out var i))
+        {
+            sb.Append(i.ToString().Replace("\"", null));
+            sb.Append(" - ");
+        }
+
+        if (logEvent.Properties.TryGetValue(LogBase.MethodProperty, out var m))
+        {
+            sb.Append(m.ToString().Replace("\"", null));
+            sb.Append(" - ");
+        }
+        
+        sb.Append(logEvent.RenderMessage());
+        DoEmit(sb.ToString());
+    }
+
+    protected abstract void DoEmit(string message);
+}
+
+public class CrestronConsoleSink : SinkBase
+{
+    protected override void DoEmit(string message) => CrestronConsole.PrintLine(message);
+}
+
+public class CrestronErrorLogSink : SinkBase
+{
+    protected override void DoEmit(string message) => ErrorLog.Error(message);
+}
