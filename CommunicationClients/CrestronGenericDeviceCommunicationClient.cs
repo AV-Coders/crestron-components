687f28edb5e022f05c7093b53e7e6015594e3a69
Anthony C <47967077+ant-car@users.noreply.github.com>
2025-08-20 00:14:05 +1000

    throw on unsupported methods

--- CommunicationClients/CrestronGenericDeviceCommunicationClient.cs
+++ CommunicationClients/CrestronGenericDeviceCommunicationClient.cs
@@ -15,9 +15,9 @@ public class CrestronGenericDeviceCommunicationClient : IpComms
         ConnectionState = args.DeviceOnLine? ConnectionState.Connected : ConnectionState.Disconnected;
     }
 
-    public override void Send(string message) { }
+    public override void Send(string message) => throw new NotSupportedException("This client does not support sending.");
 
-    public override void Send(byte[] bytes) { }
+    public override void Send(byte[] bytes) => throw new NotSupportedException("This client does not support sending.");
 
     protected override Task Receive(CancellationToken token)
     {
@@ -37,13 +37,13 @@ public class CrestronGenericDeviceCommunicationClient : IpComms
         return Task.CompletedTask;
     }
 
-    public override void SetHost(string host) { }
+    public override void SetHost(string host) => throw new NotSupportedException("This client does not support changing the host.");
 
-    public override void SetPort(ushort port) { }
+    public override void SetPort(ushort port) => throw new NotSupportedException("This client does not support changing the port.");
 
-    public override void Connect() { }
+    public override void Connect() => throw new NotSupportedException("This client does not support connecting.");
 
-    public override void Reconnect() { }
+    public override void Reconnect() => throw new NotSupportedException("This client does not support reconnecting.");
 
-    public override void Disconnect() { }
+    public override void Disconnect() => throw new NotSupportedException("This client does not support disconnecting.");
 }
