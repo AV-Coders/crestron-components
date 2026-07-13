c40974b0af34dfea2e17b01d991f3e9ef52b4166
Anthony C <47967077+ant-car@users.noreply.github.com>
2026-07-13 22:09:01 +1000

    Report generic IP devices IPs as their hosts

--- CommunicationClients/CrestronGenericDeviceCommunicationClient.cs
+++ CommunicationClients/CrestronGenericDeviceCommunicationClient.cs
@@ -5,15 +5,23 @@ namespace AVCoders.Crestron.CommunicationClients;
 
 public class CrestronGenericDeviceCommunicationClient : IpComms
 {
-    public CrestronGenericDeviceCommunicationClient(GenericDevice device) 
-        : base(String.Empty, 0, device.Description, CommandStringFormat.Ascii)
+    public CrestronGenericDeviceCommunicationClient(GenericDevice device)
+        : base(device.ConnectedIpList?.FirstOrDefault()?.DeviceIpAddress ?? String.Empty, (ushort) device.ID, device.Description, CommandStringFormat.Ascii)
     {
         device.OnlineStatusChange += HandleOnlineStatusChange;
+        if (device.ConnectedIpList != null)
+            device.IpInformationChange += HandleIpInformationChange;
     }
 
     private void HandleOnlineStatusChange(GenericBase currentDevice, OnlineOfflineEventArgs args)
     {
         ConnectionState = args.DeviceOnLine? ConnectionState.Connected : ConnectionState.Disconnected;
+    }
+
+    private void HandleIpInformationChange(GenericBase currentDevice, ConnectedIpEventArgs args)
+    {
+        if (args.Connected)
+            Host = args.DeviceIpAddress;
     }
 
     public override void Send(string message) => throw new NotSupportedException("This client does not support sending.");
